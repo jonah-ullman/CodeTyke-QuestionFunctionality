3951a879482bbe1fd25463219aa05475d0532679
Jonah Ullman <jonahmullman@gmail.com>
2020-12-21 17:08:39 -0600

    Turns selection box background blue when checked

--- src/components/selectionBox/SelectionBox.js
+++ src/components/selectionBox/SelectionBox.js
@@ -3,13 +3,22 @@ import React from 'react';
 import './Styles.scss';
 
 const SelectionBox = (props) => {
+  const [isSelected, setIsSelected] = React.useState(false)
+
+  function toggleSelectionBox() {
+    setIsSelected(!isSelected)
+  }
+
   return(
     <div
-      className="selectionBox"
+      className={`selectionBox ${isSelected ? "selectionBox--selected" : ""}`}
       id={"selectionBox" + props.id}
     >
       <img className="selectionBox__image" alt={props.answer.imageAlt} src={props.answer.image} />
-      <input className="selectionBox__checkbox" type="checkbox" />
+      <input className="selectionBox__checkbox"
+             type="checkbox"
+             onChange={toggleSelectionBox}
+      />
       <span className="selectionBox__text">{props.answer.text}</span>
     </div>
    )
